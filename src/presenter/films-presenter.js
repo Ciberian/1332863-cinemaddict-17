5e00ed3d4e4866516dc2ae344a2da1842a0133df
Ciberian <isi04047891@gmail.com>
2022-04-28 12:03:13 +0500

    Рефакторит логику рендера компонентов в films-presenter.js

--- src/presenter/films-presenter.js
+++ src/presenter/films-presenter.js
@@ -8,26 +8,45 @@ import MostCommentedFilmsView from '../view/most-commented-films-view.js';
 import { render } from '../render.js';
 
 const FILMS_AMOUNT_DISPLAYED = 5;
+const RATED_FILMS_DISPLAYED = 2;
+const COMMENTED_FILMS_DISPLAYED = 2;
 
 export default class FilmsPresenter {
   filmsSectionComponent = new FilmsSectionView();
   filmsListComponent = new FilmsListView();
-  filmsContainerComponent = new FilmsContainerView();
+  filmsContainerMainListComponent = new FilmsContainerView();
+
+  topRatedFilmsComponent = new TopRatedFilmsView();
+  filmsContainerTopListComponent = new FilmsContainerView();
+
+  mostCommentedFilmsComponent = new MostCommentedFilmsView();
+  filmsContainerCommentedListComponent = new FilmsContainerView();
 
   init = (filmsContainer) => {
     this.filmsContainer = filmsContainer;
 
     render(this.filmsSectionComponent, this.filmsContainer);
     render(this.filmsListComponent, this.filmsSectionComponent.getElement());
-    render(this.filmsContainerComponent, this.filmsListComponent.getElement());
+    render(this.filmsContainerMainListComponent, this.filmsListComponent.getElement());
 
     for (let i = 0; i < FILMS_AMOUNT_DISPLAYED; i++) {
-      render(new FilmCardView(), this.filmsContainerComponent.getElement());
+      render(new FilmCardView(), this.filmsContainerMainListComponent.getElement());
     }
 
     render(new ShowMoreBtnView(), this.filmsListComponent.getElement());
-    render(new TopRatedFilmsView(), this.filmsSectionComponent.getElement());
-    render(new MostCommentedFilmsView(), this.filmsSectionComponent.getElement());
 
+    render(this.topRatedFilmsComponent, this.filmsSectionComponent.getElement());
+    render(this.filmsContainerTopListComponent, this.topRatedFilmsComponent.getElement());
+
+    for (let i = 0; i < RATED_FILMS_DISPLAYED; i++) {
+      render(new FilmCardView(), this.filmsContainerTopListComponent.getElement());
+    }
+
+    render(this.mostCommentedFilmsComponent, this.filmsSectionComponent.getElement());
+    render(this.filmsContainerCommentedListComponent, this.mostCommentedFilmsComponent.getElement());
+
+    for (let i = 0; i < COMMENTED_FILMS_DISPLAYED; i++) {
+      render(new FilmCardView(), this.filmsContainerCommentedListComponent.getElement());
+    }
   };
 }
