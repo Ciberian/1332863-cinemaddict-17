c8f3c66ed271fe6b16e25b92342de092f2bbe9ce
Ciberian <isi04047891@gmail.com>
2022-05-08 18:47:44 +0500

    Редактирует константу в модуле const.js

--- src/const.js
+++ src/const.js
@@ -24,7 +24,6 @@ const DEFAULT_GENRES = ['Drama', 'Comedy', 'Musical', 'Western', 'Cartoon'];
 const EMOTIONS = ['smile', 'sleeping', 'puke', 'angry'];
 
 const FilterType = {
-  ALL: 'all',
   WATCHLIST: 'watchlist',
   HISTORY: 'history',
   FAVORITES: 'favorites',
